222e29db026370c624c5ea57795cc9888387b756
Volchenkov Pavel Nikolaevich <162349881+PshaViXzzz@users.noreply.github.com>
2024-03-24 21:17:17 +0300

    Update Main.java

--- src/main/java/ru/imagine/Main.java
+++ src/main/java/ru/imagine/Main.java
@@ -2,9 +2,9 @@ package ru.imagine;
 
 public class Main {
 
-private static String msg = "Hello qa.guru!!!!!!!!!!!!!!!!!!!!!!";
+private static String msg = "Hello qa.guru!!!";
 
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru");
+        System.out.println(msg);
     }
 }
