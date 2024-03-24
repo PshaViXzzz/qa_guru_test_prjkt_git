a154b5fde6fbabb365f6cc3a46c89afa3ea8bffb
Pavel Volchenkov <imaginetreasure@gmail.com>
2024-03-24 21:25:42 +0300

    test2

--- src/main/java/ru/imagine/Main.java
+++ src/main/java/ru/imagine/Main.java
@@ -2,9 +2,7 @@ package ru.imagine;
 
 public class Main {
 
-private static String msg = "Hello qa.guru!!!11111!!!!!!";
-
     public static void main(String[] args) {
-        System.out.println(msg);
+        System.out.println("Hello QA.guru!");
     }
 }
